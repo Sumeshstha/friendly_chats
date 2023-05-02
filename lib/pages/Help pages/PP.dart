95e1ebafa4b2aabfcc70902c04fe32275777fc60
Bluephoenixsama <adhikarybad@gmail.com>
2023-05-02 18:47:50 +0545

    baki xa ekxin

---
+++ lib/pages/Help pages/PP.dart
@@ -0,0 +1,20 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+
+class Faq extends StatefulWidget {
+  const Faq({Key? key}) : super(key: key);
+
+  @override
+  State<Faq> createState() => _FaqState();
+}
+
+class _FaqState extends State<Faq> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("sup"),
+      ),
+    );
+  }
+}
